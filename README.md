f0dde5858b8a445771a35d55daecb06a76e1a4aa
Darshit Ambasana <darshit.ambasana104847@marwadiuniversity.ac.in>
2020-05-03 12:34:27 +0530

    Creating initial changes to files.

---
+++ README.md
@@ -0,0 +1,5 @@
+/* Author: Darshit Ambasana
+Project: Statistical operations on an array of unsigned characters,
+the operations like mean,median,sorting in descending oreder,etc. 
+to the array of certain length. We make fuction of all the operations
+to be done including printing the array and output of operations. */
---
+++ stats.h
@@ -0,0 +1,41 @@
+/******************************************************************************
+ * Copyright (C) 2017 by Alex Fosdick - University of Colorado
+ *
+ * Redistribution, modification or use of this software in source or binary
+ * forms is permitted as long as the files maintain this copyright. Users are 
+ * permitted to modify this and use it to learn about the field of embedded
+ * software. Alex Fosdick and the University of Colorado are not liable for any
+ * misuse of this material. 
+ *
+ *****************************************************************************/
+/**
+ * @file stats.h 
+ * @brief Basic statistics on an array.
+ *
+ * The given array is of fixed length and of unsigned character type.
+ * Some operations to be done are finding mean,median,sorting array in descending order,etc.
+ *
+ * @author Darshit Ambasana
+ * @date 3/5/2020
+ *
+ */
+#ifndef __STATS_H__
+#define __STATS_H__
+
+/* Add Your Declarations and Function Comments here */ 
+
+/**
+ * @brief <Add Brief Description of Function Here>
+ *
+ * <Add Extended Description Here>
+ *
+ * @param <Add InputName> <add description here>
+ * @param <Add InputName> <add description here>
+ * @param <Add InputName> <add description here>
+ * @param <Add InputName> <add description here>
+ *
+ * @return <Add Return Informaiton here>
+ */
+
+
+#endif /* __STATS_H__ */
